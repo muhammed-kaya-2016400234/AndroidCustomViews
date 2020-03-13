191e285b53694adc89e829f799335bfc64b41e2e
muhammed-kaya-2016400234 <kaya.bera@gmail.com>
2020-03-13 18:12:10 +0300

    mask functionality implemented

--- customcomps/src/main/java/com/example/customcomps/UyumEditText.java
+++ customcomps/src/main/java/com/example/customcomps/UyumEditText.java
@@ -1,12 +1,14 @@
 package com.example.customcomps;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
 import android.util.AttributeSet;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -18,6 +20,7 @@ import android.widget.LinearLayout;
 import com.example.customcomps.helpers.TextValidation;
 import com.example.customcomps.helpers.UyumConstants;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Vector;
 
@@ -27,6 +30,9 @@ public class UyumEditText extends LinearLayout  {
     public EditText editText;
 
     public int type=3;
+
+
+    public final Vector<Character> maskChars=new Vector<>(Arrays.asList('#','L'));
 
     public UyumEditText(Context context, AttributeSet attributeSet){
         super(context,attributeSet);
@@ -89,6 +95,7 @@ public class UyumEditText extends LinearLayout  {
 
     public void setType(int type){
         this.type=type;
+        editText.setRawInputType(Configuration.KEYBOARD_QWERTY);
         if(type== UyumConstants.EditTextTypes.Password){
             img.setVisibility(VISIBLE);
             editText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
@@ -96,12 +103,14 @@ public class UyumEditText extends LinearLayout  {
             img.setVisibility(GONE);
             if(type==UyumConstants.EditTextTypes.Name){
                 editText.setInputType(InputType.TYPE_CLASS_TEXT);
-            }else if(type==UyumConstants.EditTextTypes.Phone||type==UyumConstants.EditTextTypes.TCKN){
-                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+            }else if(type==UyumConstants.EditTextTypes.Phone){
+                editText.setInputType(InputType.TYPE_CLASS_PHONE);
             }else if(type==UyumConstants.EditTextTypes.EMail){
                 editText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
             }else if(type==UyumConstants.EditTextTypes.Date){
                 editText.setInputType(InputType.TYPE_CLASS_DATETIME);
+            }else if(type==UyumConstants.EditTextTypes.TCKN){
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
             }
 
         }
@@ -130,11 +139,16 @@ public class UyumEditText extends LinearLayout  {
     public void setMask(final String mask){
         editText.setText("");
         final Vector<Integer> vec=new Vector<>();
+        int firstInputIndex=-1;
+
         for(int i=0;i<mask.length();i++){
             char c=mask.charAt(i);
-            if(c=='#'||c=='L'){
+            if(maskChars.contains(c)){
+                if(firstInputIndex==-1){
+                    firstInputIndex=i;
+                }
                 vec.add(i);
-                editText.setText(editText.getText()+"_");
+                editText.setText(editText.getText()+"-");
             }else{
                 editText.setText(editText.getText()+String.valueOf(c));
             }
@@ -154,15 +168,28 @@ public class UyumEditText extends LinearLayout  {
             }
         });
 
+        final Vector<Character> chars=new Vector<>();
+        final Vector<Character> writtenChars=new Vector<>();
+        final Vector<Integer> deletedIndices=new Vector<>();
+
+        final int finalFirstInputIndex = firstInputIndex;
         editText.addTextChangedListener(new TextWatcher() {
 
             String textbefore="";
             String textafter="";
             int index=0;
+            int dontcareindex=-1;
+            String beforechange="";
+            int selectlocation=0;
+
+            int changedIndex=-1;
+            boolean skip=false;
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 textbefore=charSequence.toString();
-                index=i1;
+                index=i;
+                beforechange=charSequence.toString();
+
             }
 
             @Override
@@ -172,20 +199,166 @@ public class UyumEditText extends LinearLayout  {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(editable.length()>index+1){
-                    if(editable.charAt(index+1)=='_'){
-                        editable.delete(index+1,5);
+
+                /*
+                    if (editable.length() > index + 1) {
+                        if (index<mask.length()&&mask.charAt(index) == '#') {
+                            if (index != dontcareindex) {
+                                dontcareindex = 0;
+                                CharSequence s1 = editable.subSequence(0, index + 1);
+                                CharSequence s = editable.subSequence(index + 2, editable.length());
+                                String str = s1.toString() + s.toString();
+                                vec.remove((Integer)index);
+                                editable.replace(0, editable.length(), s1.toString() + s.toString());
+                                if(vec.size()>0)
+                                    editText.setSelection(Collections.min(vec));
+                                else
+                                    editText.setSelection(mask.length());
+                                int b=4;
+                            } else {
+                                dontcareindex = -1;
+                            }
+                        }
+                    }
+                    if(editable.length()>mask.length()){
+                        dontcareindex=0;
+                        editable.replace(0,editable.length(),beforechange);
+                    }
+
+                 */
+
+
+                if(!skip) {
+                    if(chars.size()>0) {
+                        if(writtenChars.size()!=vec.size())
+                            writtenChars.addAll(chars);
+                        int z = 0;
+                        StringBuilder replacement = new StringBuilder();
+                        int selectlocation = 0;
+                        boolean sel=true;
+                        for (int i = 0; i < mask.length(); i++) {
+
+                            if (z < writtenChars.size() && maskChars.contains(mask.charAt(i))) {
+
+                                replacement.append(writtenChars.get(z).toString());
+                                z++;
+
+
+                            } else {
+                                if (maskChars.contains(mask.charAt(i))) {
+                                    replacement.append("-");
+                                    if(sel&&writtenChars.size()!=0){
+                                        selectlocation=i;
+                                        sel=false;
+                                    }
+                                } else {
+                                    replacement.append(mask.charAt(i));
+                                }
+
+                            }
+                        }
+                        skip = true;
+                        editable.delete(0, editable.length());
+                        editable.append(replacement.toString());
+                        if(writtenChars.size()==vec.size())
+                            editText.setSelection(editable.length());
+                        else
+                            editText.setSelection(selectlocation);
+                        skip = false;
+                        chars.clear();
+                    }
+                    if(deletedIndices.size()>0){
+                        /*
+                        int ind=-1;
+                        for(int i=0;i<=deletedIndices.get(0);i++){
+                            if(maskChars.contains(mask.charAt(i))){
+                                ind++;
+                            }
+
+                        }
+
+                         */
+                            //writtenChars.insertElementAt('-',ind);
+                            if(writtenChars.size()>0)
+                                writtenChars.remove(writtenChars.size()-1);
+                            //chars.clear();
+                            //chars.addAll(writtenChars);
+                           // writtenChars.clear();
+                            StringBuilder replacement = new StringBuilder();
+                            int selectlocation = 0;
+                            int z=0;
+                            boolean sel=true;
+                            for (int i = 0; i < mask.length(); i++) {
+
+                                if (z < writtenChars.size() && maskChars.contains(mask.charAt(i))) {
+
+                                    replacement.append(writtenChars.get(z).toString());
+                                    //editable.replace(i,1,a);
+                                    z++;
+                                    /*
+                                    if (z == writtenChars.size()) {
+                                        //selectlocation = i + 1;
+                                        sel=true;
+                                    }
+
+                                     */
+
+                                } else {
+                                    if (maskChars.contains(mask.charAt(i))) {
+                                        replacement.append("-");
+                                        if(sel&&writtenChars.size()!=0){
+                                            selectlocation=i;
+                                            sel=false;
+                                        }
+                                    } else {
+
+                                        replacement.append(mask.charAt(i));
+                                    }
+                                    //editable.replace(i,1,mask.charAt(i)+"");
+                                    //editable.delete(0,editable.length()-1);
+                                    //Editable d=editable;
+                                }
+                            }
+                            skip = true;
+                            editable.delete(0, editable.length());
+                            editable.append(replacement.toString());
+                            if(writtenChars.size()==0)
+                                editText.setSelection(finalFirstInputIndex);
+                            else
+                                editText.setSelection(selectlocation);
+                            skip = false;
+
+                        deletedIndices.clear();
+                        chars.clear();
                     }
                 }
-                String s=editable.toString();
+
+
 
             }
         });
+
         InputFilter filter = new InputFilter() {
             public CharSequence filter(CharSequence source, int start, int end,
                                        Spanned dest, int dstart, int dend) {
 
-                if(source.toString().length()==1){
+                boolean valid=true;
+
+                if(valid) {
+                    if (chars.size() == 0) {
+                        for (int i = 0; i < source.length(); i++) {
+                            chars.add(source.charAt(i));
+                        }
+                        if (source == "" && deletedIndices.size() == 0) {
+                            deletedIndices.add(dstart);
+                        }
+                    }
+                }else{
+                    return "";
+                }
+
+
+               // if(source.toString().length()==1){
                     /*
                     StringBuffer stringBuffer=new StringBuffer(dest);
                     String s="";
@@ -202,17 +375,55 @@ public class UyumEditText extends LinearLayout  {
                        return null;
                     }
 
-                     */
-                    return null;
+
+                    return null;*/
+               // }
+                /*
+                if(dstart>=mask.toString().length()){
+                    return "";
+                    // s=stringBuffer.insert(dstart,source).toString();
                 }
-                return "";
+
+                 */
+                return null;
 
             }
         };
 
         editText.setFilters(new InputFilter[] { filter });
     }
+    /*
+    private String getReplacement(String mask,Vector<Character> writtenChars,){
+        String replacement="";
+        for (int i = 0; i < mask.length(); i++) {
 
+            if (z < writtenChars.size() && maskChars.contains(mask.charAt(i))) {
+
+                replacement += writtenChars.get(z).toString();
+                //editable.replace(i,1,a);
+                z++;
+                if (z == writtenChars.size()) {
+                    selectlocation = i + 1;
+                }
+
+            } else {
+                if (maskChars.contains(mask.charAt(i))) {
+                    replacement += "_";
+                } else {
+                    replacement += mask.charAt(i);
+                }
+                //editable.replace(i,1,mask.charAt(i)+"");
+                //editable.delete(0,editable.length()-1);
+                //Editable d=editable;
+            }
+        }
+    }
+
+     */
+
+    private void calculateSelectionIndex(){
+
+    }
     public boolean validateInput(String s){
 
         if(type==UyumConstants.EditTextTypes.EMail){
